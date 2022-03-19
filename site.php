9eaaa5cd4aa4917d52789c91c9144e216486c22f
ruan <araujoruan456@gmail.com>
2022-03-19 12:24:07 -0300

    Carrinho de Compras

--- site.php
+++ site.php
@@ -3,6 +3,11 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
+use \Hcode\Model\Address;
+use \Hcode\Model\User;
+use \Hcode\Model\Order;
+use \Hcode\Model\OrderStatus;
 
 $app->get('/', function() {
 
@@ -59,5 +64,16 @@ $app->get("/products/:desurl", function($desurl){
 	]);
 
 });
+
+$app->get("/cart", function(){
+
+	$cart = Cart::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("cart");
+
+});
+
 
 ?>
