3f6dbe5b2bb3852f984063429e2e6be478191802
ruan <araujoruan456@gmail.com>
2022-03-27 09:38:06 -0300

    Site Login

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -243,15 +243,15 @@ class User extends Model {
 
 				if ($inadmin === true) {
 
-					$link = "http://www.hcoderuan.com.br/admin/forgot/reset?code=$code";
+					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
 
 				} else {
 
-					$link = "http://www.hcoderuan.com.br/forgot/reset?code=$code";
+					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
 					
 				}				
 
-				$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Loja Daupause", "forgot", array(
+				$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Hcode Store", "forgot", array(
 					"name"=>$data['desperson'],
 					"link"=>$link
 				));				
@@ -265,65 +265,109 @@ class User extends Model {
 		}
 
 	}
-  public static function validForgotDecrypt($code)
-  {
-    $code = base64_decode($code);
-    $idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
 
-    $sql = new Sql();
+	public static function validForgotDecrypt($code)
+	{
 
-    $results = $sql->select("
-      SELECT *
-      FROM tb_userspasswordsrecoveries a
-      INNER JOIN tb_users b USING(iduser)
-      INNER JOIN tb_persons c USING(idperson)
-      WHERE
-        a.idrecovery = :idrecovery
-        AND
-        a.dtrecovery IS NULL
-        AND
-        DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
-    ", array(
-      ":idrecovery"=>$idrecovery
-    ));
+		$code = base64_decode($code);
 
-    if (count($results) === 0)
-    {
-      throw new \Exception("Não foi possível recuperar a senha.");
-    }
-    else
-    {
+		$idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
 
-      return $results[0];
+		$sql = new Sql();
 
-    }
-  }
+		$results = $sql->select("
+			SELECT *
+			FROM tb_userspasswordsrecoveries a
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE
+				a.idrecovery = :idrecovery
+				AND
+				a.dtrecovery IS NULL
+				AND
+				DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+		", array(
+			":idrecovery"=>$idrecovery
+		));
 
-public static function setFogotUsed($idrecovery)
-  {
-    $sql = new Sql();
+		if (count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else
+		{
 
-    $sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
-      ":idrecovery"=>$idrecovery
-    ));
-  }
+			return $results[0];
 
-public function setPassword($password){
-  $sql = new Sql();
-  $sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
-     ":password"=>$password,
-     ":iduser"=>$this->getiduser()
-  ));
-}
+		}
 
-public static function getPasswordHash($password)
-  {
+	}
+	
+	public static function setFogotUsed($idrecovery)
+	{
 
-    return password_hash($password, PASSWORD_DEFAULT, [
-      'cost'=>12
-    ]);
+		$sql = new Sql();
 
-  }
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+			":idrecovery"=>$idrecovery
+		));
+
+	}
+
+	public function setPassword($password)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+			":password"=>$password,
+			":iduser"=>$this->getiduser()
+		));
+
+	}
+
+	public static function setError($msg)
+	{
+
+		$_SESSION[User::ERROR] = $msg;
+
+	}
+
+	public static function getError()
+	{
+
+		$msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : '';
+
+		User::clearError();
+
+		return $msg;
+
+	}
+
+	public static function clearError()
+	{
+
+		$_SESSION[User::ERROR] = NULL;
+
+	}
+
+	public static function setSuccess($msg)
+	{
+
+		$_SESSION[User::SUCCESS] = $msg;
+
+	}
+
+	public static function getPasswordHash($password)
+	{
+
+		return password_hash($password, PASSWORD_DEFAULT, [
+			'cost'=>12
+		]);
+
+	}
+
+
 
 }
 
